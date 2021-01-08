24029b43044e94e149afe2b5a23103caedab6489
sergiu nimat <stud.demy@gmail.com>
2021-01-08 11:25:08 +0100

    Final Base demo ReactJs project.

--- paypal-demo-app/src/App.js
+++ paypal-demo-app/src/App.js
@@ -1,5 +1,5 @@
 
-import {Col,Row,Card, Button,Menu,Dropdown} from 'antd'
+import {Col,Row,Card, Button,Modal,Dropdown,Table} from 'antd'
 import './App.css';
 import 'antd/dist/antd.css';
 import { useEffect, useState } from 'react'
@@ -7,6 +7,7 @@ import { useEffect, useState } from 'react'
 const { Meta } = Card;
 
 function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
   const [products, setProducts] = useState('')
   const [cart, setCart] = useState([])
 
@@ -30,6 +31,67 @@ function App() {
     setCart(tempCart)
   }
 
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+
+  //#region  Table columns 
+
+  const columns = [
+    {
+      title: 'Product name',
+      dataIndex: 'name',
+    },
+    {
+      title: 'Price',
+      dataIndex: 'price',
+    },
+   
+  ];
+  const data = [
+    {
+      key: '1',
+      name: 'John Brown',
+      price: 32,      
+      description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.',
+      amount:{
+        currency_code:'EUR',
+        value:100.00
+      }
+    },
+    {
+      key: '2',
+      name: 'John Brown',
+      price: 32,      
+      description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.',
+      amount:{
+        currency_code:'EUR',
+        value:100.00
+      }
+    },
+    {
+      key: '3',
+      name: 'John Brown',
+      price: 32,      
+      description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.',
+      amount:{
+        currency_code:'EUR',
+        value:100.00
+      }
+    }
+  ];
+
+  //#endregion
+
 
 
   return (
@@ -39,7 +101,11 @@ function App() {
         <Col span={12}>
           <h1>PayPal Demo</h1> 
         </Col>
-        <Col span={12}><Button>Check out {cart.length}</Button></Col>
+        <Col span={12}>          
+          {/* disabled={cart.length===0?true:false} -- this is a coding option */}
+          <Button onClick={showModal}>Show cart content</Button> 
+        </Col>
+        
       {products===''?<h1>There are no products to show</h1>:products.map(p=>{
         return(<Col key={p.id} height={150} span={3}>          
           <Card
@@ -52,6 +118,28 @@ function App() {
           </Card>
         </Col>);
       })}
+
+      
+      <Modal  title={`you have (${cart.length}) elements in the cart.`} 
+                  visible={isModalVisible}
+                  onCancel={handleCancel}                   
+                  footer={[
+                    <Button key="back" onClick={handleCancel}>
+                      Return
+                    </Button>,
+                    <Button key="submit" type="primary" disabled={cart.length===0?true:false} onClick={handleOk}>
+                      Checkout
+                    </Button>,
+                  ]}>
+        <Table columns={columns} 
+                  dataSource={data} 
+                  expandable={{
+                    expandedRowRender: record => <p style={{ margin: 0 }}>{record.description}</p>,
+                    rowExpandable: record => record.name !== 'Not Expandable',
+                  }}
+                  pagination={false}
+                  size="small" />
+      </Modal>
       </Row>     
     </div>
   );
